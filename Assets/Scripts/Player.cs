81cc3c976f87af29814eb4fa2874e30b8c74953f
Ion Ion <sabau_lorand@yahoo.com>
2024-03-26 15:18:24 +0200

    Side movement and Layers

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,14 @@ public class Player : MonoBehaviour
     public Vector3 jump;
     public float jumpForce = 2.0f;
     public bool isGrounded;
+    private float horizontalInput;
+    private Rigidbody rigidBodyComponent;
 
     // Start is called before the first frame update
     void Start()
     {
         jump = new Vector3(0.0f, 2.0f, 0.0f);
+        rigidBodyComponent = GetComponent<Rigidbody>();
     }
 
      void OnCollisionStay()
@@ -26,8 +29,15 @@ public class Player : MonoBehaviour
         // Check if space key is pressed down
         if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
-            GetComponent<Rigidbody>().AddForce(jump * jumpForce, ForceMode.Impulse);
+            rigidBodyComponent.AddForce(jump * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBodyComponent.velocity = new Vector3(horizontalInput * jumpForce, rigidBodyComponent.velocity.y, rigidBodyComponent.velocity.z);
     }
 }
